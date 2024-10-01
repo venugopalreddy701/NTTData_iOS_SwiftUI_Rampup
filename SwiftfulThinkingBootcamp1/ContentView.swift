27b45fde238fd311f2bce98473eaeb3b0af3a825
Venugopal Reddy <venugopalreddy701@gmail.com>
2024-10-01 11:07:03 +0530

    Initial Commit

---
+++ SwiftfulThinkingBootcamp1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulThinkingBootcamp1
+//
+//  Created by Venugopal on 01/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulThinkingBootcamp1/SwiftfulThinkingBootcamp1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootcamp1App.swift
+//  SwiftfulThinkingBootcamp1
+//
+//  Created by Venugopal on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootcamp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
