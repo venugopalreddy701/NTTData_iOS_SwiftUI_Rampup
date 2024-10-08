638d5c0a98c823afd009d3f2f38acedfa7b71f72
Venugopal Reddy <venugopalreddy701@gmail.com>
2024-10-08 13:14:09 +0530

    V21:How to extract subviews

--- SwiftfulThinkingBootcamp1/ExtractSubViewBootcamp.swift
+++ SwiftfulThinkingBootcamp1/ExtractSubViewBootcamp.swift
@@ -11,12 +11,43 @@ struct ExtractSubViewBootcamp: View {
     
     var body: some View {
        
-        
+        ZStack{
+            Color(Color.blue).edgesIgnoringSafeArea(.all)
+            
+            contentLayer
+        }
         
     }
+    var contentLayer: some View {
+        HStack{
+            MyItem(title: "Apples", count: 1, color: .red)
+            MyItem(title: "Oranges", count: 10, color: .orange)
+            MyItem(title: "Bananas", count: 34 , color: .yellow)
+        }
+    }
+    
+  
     
 }
 
 #Preview {
     ExtractSubViewBootcamp()
+}
+
+struct MyItem: View {
+    
+    let title: String
+    let count: Int
+    let color: Color
+    
+    
+    var body: some View {
+        VStack{
+            Text("\(count)")
+            Text(title)
+        }
+        .padding()
+        .background(color)
+        .cornerRadius(10)
+    }
 }
