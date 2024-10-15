c6e13d2539374de429c9843c9dc869354812ed0e
Venugopal Reddy <venugopalreddy701@gmail.com>
2024-10-15 22:44:15 +0530

    V50: @Published and @StateObject, @ObservedObject

---
+++ SwiftfulThinkingBootcamp1/EnvironmentObjectBootcamp.swift
@@ -0,0 +1,22 @@
+//
+//  EnvironmentObjectBootcamp.swift
+//  SwiftfulThinkingBootcamp1
+//
+//  Created by Venugopal on 15/10/24.
+//
+
+import SwiftUI
+
+struct EnvironmentObjectBootcamp: View {
+    
+    var body: some View {
+       
+        
+    }
+    
+    
+}
+
+#Preview {
+    EnvironmentObjectBootcamp()
+}
