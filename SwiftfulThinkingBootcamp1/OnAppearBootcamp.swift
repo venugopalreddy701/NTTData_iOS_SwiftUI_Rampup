e2bee1e6c559bd2719666b8fc98e2f6afecefe99
Venugopal Reddy <venugopalreddy701@gmail.com>
2024-10-15 15:33:03 +0530

    V46:OnAppear and OnDisapper

---
+++ SwiftfulThinkingBootcamp1/OnAppearBootcamp.swift
@@ -0,0 +1,53 @@
+//
+//  OnAppearBootcamp.swift
+//  SwiftfulThinkingBootcamp1
+//
+//  Created by Venugopal on 15/10/24.
+//
+
+import SwiftUI
+
+struct OnAppearBootcamp: View {
+    
+    @State var myText:String = "Start text"
+    @State var count: Int = 0
+    var body: some View {
+        
+        NavigationView{
+            ScrollView{
+                Text(myText)
+                LazyVStack{
+                    ForEach(0..<50){ _ in
+                        RoundedRectangle(cornerRadius: 25.0)
+                            .frame(height: 200)
+                            .padding()
+                            .onAppear{
+                                count += 1
+                            }
+                           
+                    }
+                }
+            }
+            .onAppear(perform: {
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5){
+                    
+                    myText = "This is the new text!"
+                }
+                
+                
+            })
+            .onDisappear(perform: {
+                myText = "Ending Text"
+            })
+            .navigationTitle("On Appear \(count)")
+            
+        }
+        
+        
+    }
+}
+
+#Preview {
+    OnAppearBootcamp()
+}
